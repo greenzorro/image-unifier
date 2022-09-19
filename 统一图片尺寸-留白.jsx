38ee4e88f43b2777d3a2b18a73b7b8fe117b44a1
Victor_42 <greenzorromail@gmail.com>
2022-09-19 10:21:44 +0800

    Add files via upload

---
+++ 统一图片尺寸-留白.jsx
@@ -0,0 +1,28 @@
+#target photoshop;
+app.bringToFront();
+var targetWidth = parseInt(prompt("请输入目标宽度"));
+var targetHeight = parseInt(prompt("请输入目标高度"));
+var ratio = targetWidth/targetHeight;
+var exportFolder = Folder.selectDialog("请选择导出位置")
+
+var jpgSaveOptions = new JPEGSaveOptions();
+jpgSaveOptions.embedColorProfile = true;
+jpgSaveOptions.formatOptions = FormatOptions.STANDARDBASELINE;
+jpgSaveOptions.matte = MatteType.NONE;
+jpgSaveOptions.quality = 12;
+
+while (documents.length > 0) {
+	var currentDoc = app.activeDocument;
+	currentDoc.resizeImage(undefined, undefined, 72, ResampleMethod.NONE);
+	var currentWidth = currentDoc.width;
+	var currentHeight = currentDoc.height;
+	if (currentWidth/currentHeight > ratio) {
+		currentDoc.resizeImage(targetWidth, undefined);
+	}
+	else {
+		currentDoc.resizeImage(undefined, targetHeight);
+	}
+	currentDoc.resizeCanvas(targetWidth, targetHeight);
+	currentDoc.saveAs(new File(exportFolder + "/" + currentDoc.name.match(/(.*)\.[^\.]+$/)[1] + ".jpg"), jpgSaveOptions, true, Extension.LOWERCASE);
+	currentDoc.close(SaveOptions.DONOTSAVECHANGES);
+}
